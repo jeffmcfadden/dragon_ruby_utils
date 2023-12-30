a992e2a7229b777fc773af9c2cc25908cc6afb8f
Jeff McFadden <jeffmcfadden@users.noreply.github.com>
2023-12-30 13:31:40 -0700

    Fix some bugs in nice slices

--- nine_slice.rb
+++ nine_slice.rb
@@ -25,7 +25,7 @@ def nine_slice(args, path:, x1:, x2:, y1:, y2:, w:, h:, output_x:, output_y:, ou
 
   lower_left_slice = {x: 0, y: y2, w: x1, h: h - y2}
   lower_middle_slice = {x: x1, y: y2, w: x2 - x1, h: h - y2}
-  lower_right_slice = {x: x2, y: y2, w: h - x2, h: h - y2}
+  lower_right_slice = {x: x2, y: y2, w: w - x2, h: h - y2}
 
   # Calculate the top Y value. Our coordinates are flipped between the sprite and the output, so we need to flip the Y value.
   top_y = output_y + output_h - y1
@@ -44,7 +44,7 @@ def nine_slice(args, path:, x1:, x2:, y1:, y2:, w:, h:, output_x:, output_y:, ou
   args.outputs.sprites << {x:output_x, y:middle_y, w:middle_left_slice.w, h:output_h - upper_left_slice.h - lower_left_slice.h, path:path, tile_x:0, tile_y:upper_left_slice.h, tile_w:middle_left_slice.w, tile_h:middle_left_slice.h}
 
   # Middle middle sprite is a stretched sprite
-  args.outputs.sprites << {x:output_x + middle_left_slice.w, y:middle_y, w:output_w - middle_left_slice.w - middle_right_slice.w, h:output_h - upper_middle_slice.h - lower_middle_slice.h, path:path, tile_x:middle_left_slice.w, tile_y:middle_left_slice.h, tile_w:middle_middle_slice.w, tile_h:middle_middle_slice.h}
+  args.outputs.sprites << {x:output_x + middle_left_slice.w, y:middle_y, w:output_w - middle_left_slice.w - middle_right_slice.w, h:output_h - upper_middle_slice.h - lower_middle_slice.h, path:path, tile_x:middle_middle_slice.x, tile_y:middle_middle_slice.y, tile_w:middle_middle_slice.w, tile_h:middle_middle_slice.h}
 
   # Middle right sprite is a stretched sprite
   args.outputs.sprites << {x:output_x + output_w - middle_right_slice.w, y:middle_y, w:middle_right_slice.w, h:output_h - upper_right_slice.h - lower_right_slice.h, path:path, tile_x:middle_right_slice.x, tile_y:middle_right_slice.y, tile_w:middle_right_slice.w, tile_h:middle_right_slice.h}
